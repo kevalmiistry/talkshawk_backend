5ea02985bf17f3274fdebb43fb55626026e08d37
Keval <kevalpm@gmail.com>
2022-11-18 11:40:27 +0530

    ALl message related routes completed

--- src/controllers/messageControllers.ts
+++ src/controllers/messageControllers.ts
@@ -1,3 +1,55 @@
+import { Request, RequestHandler, Response } from 'express'
 import asyncHandler from 'express-async-handler'
+import Chat from '../models/chatModel'
+import Message from '../models/messageModel'
+import User from '../models/userModel'
 
-// export const
+export const createMessage: RequestHandler = asyncHandler(
+    async (req: Request, res: Response) => {
+        const { _id } = res.locals
+        const { content, chatId } = req.body
+
+        if (!content || !chatId) {
+            res.json({ success: false, message: 'Invalid data passed!' })
+            return
+        }
+        const newMessage = {
+            sender: _id,
+            content,
+            chat: chatId,
+        }
+
+        try {
+            let message = await Message.create(newMessage)
+            message = await message.populate('sender', 'name username pic')
+            message = await message.populate('chat')
+            const finalMessage = await User.populate(message, {
+                path: 'chat.users',
+                select: 'name username pic email',
+            })
+
+            await Chat.findByIdAndUpdate(chatId, {
+                latestMessage: finalMessage,
+            })
+
+            res.json(finalMessage)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+)
+
+export const fetchMessages: RequestHandler = asyncHandler(
+    async (req: Request, res: Response) => {
+        try {
+            const messages = await Message.find({
+                chat: req.params.chatId,
+            })
+                .populate('sender', 'name username email pic')
+                .populate('chat')
+            res.json(messages)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+)
--- src/routes/messageRoutes.ts
+++ src/routes/messageRoutes.ts
@@ -1,4 +1,8 @@
 import { Router } from 'express'
+import { createMessage, fetchMessages } from '../controllers/messageControllers'
+import fetchUser from '../utils/fetchUser'
 export const router = Router()
 
-router.post('/')
+router.post('/create', fetchUser, createMessage)
+
+router.get('/:chatId', fetchUser, fetchMessages)
