763dc6ad412fddecdf61264f166b532d00a75405
Keval <kevalpm@gmail.com>
2022-12-02 15:19:41 +0530

    Sockets are implemented!

---
+++ src/Socket/Sockets.ts
@@ -0,0 +1,75 @@
+import { Server } from 'socket.io'
+import { DefaultEventsMap } from 'socket.io/dist/typed-events'
+
+type TUserData = {
+    _id: string
+    name: string
+    username: string
+    email: string
+    pic: string
+}
+type TMessageSender = {
+    _id: string
+    pic: string
+    name: string
+    username: string
+}
+type TMessage = {
+    _id: string
+    sender: TMessageSender
+    content: string
+    chat: SingleChatData
+}
+type SingleChatData = {
+    _id: string
+    chatName: string
+    isGroupChat: boolean
+    groupPic: string
+    users: TUserData[]
+    groupAdmins: TUserData[]
+    latestMessage?: TMessage
+}
+
+const configureSockets = (
+    io: Server<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>
+) => {
+    io.on('connection', (socket) => {
+        console.log('CONNECTED TO SOCKET.IO')
+
+        socket.on('joinChat', (roomId) => {
+            socket.join(roomId)
+        })
+
+        socket.on('online', (userData: TUserData) => {
+            socket.join(userData._id)
+            console.log('The User Joined: ' + userData._id)
+            socket.emit('__online__')
+        })
+
+        socket.on('typing', (roomId, pic) =>
+            socket.broadcast.in(roomId).emit('typing', pic)
+        )
+        socket.on('stopTyping', (roomId) =>
+            socket.broadcast.in(roomId).emit('stopTyping')
+        )
+
+        socket.on('newMessage', (newMessage: TMessage) => {
+            let chat = newMessage.chat
+            if (!chat.users) return console.error('chat.users not found')
+            socket.broadcast.in(chat._id).emit('messageRecieved', newMessage)
+
+            chat.users.forEach((u: TUserData) => {
+                socket
+                    .in(u._id)
+                    .emit('commonMessageRecieved', newMessage, u._id)
+            })
+        })
+
+        socket.off('setUp', (userData: TUserData) => {
+            console.log('USER DISCONNECTED')
+            socket.leave(userData._id)
+        })
+    })
+}
+
+export default configureSockets
--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,8 @@ import connectToDB from './config/db'
 import { router as userRouter } from './routes/userRoutes'
 import { router as chatRouter } from './routes/chatRoutes'
 import { router as messageRouter } from './routes/messageRoutes'
-
+import { Server } from 'socket.io'
+import configureSockets from './Socket/Sockets'
 dotenv.config()
 
 const app = express()
@@ -31,6 +32,15 @@ app.use('/api/user', userRouter)
 app.use('/api/chat', chatRouter)
 app.use('/api/message', messageRouter)
 
-app.listen(PORT, () =>
+const server = app.listen(PORT, () =>
     console.log('Server is running at http://localhost:' + PORT)
 )
+
+const io = new Server(server, {
+    pingTimeout: 60000,
+    cors: {
+        origin: process.env.CLIENT_URL,
+    },
+})
+
+configureSockets(io)
